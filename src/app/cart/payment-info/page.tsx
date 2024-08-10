e9ffc20dfca809cd7e2f70d6f4b971be89e62fe2
Quang Phan <quang11102002f@gmail.com>
2024-08-10 20:37:38 +0700

    build

--- src/app/cart/payment-info/page.tsx
+++ src/app/cart/payment-info/page.tsx
@@ -4,7 +4,7 @@ import { useStoreCartPriview } from "@/Lib/Store/StorecartPriview";
 import type { TabsProps } from "antd";
 import { Tabs } from "antd";
 import { Form, Formik } from "formik";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import InforPayment from "./Children/InforPayment";
 import Payment from "./Children/Payment";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -30,78 +30,80 @@ const PaymentInfo = () => {
   const onSubmit = (values: any) => {};
   return (
     <div className="payment-tab">
-      <Formik initialValues={{}} onSubmit={onSubmit}>
-        {(props) => {
-          const { handleChange, values } = props;
-          const items: TabsProps["items"] = [
-            {
-              key: TabsKey.Info,
-              label: "1. Thong tin",
-              children: <InforPayment handleChange={handleChange} />,
-            },
-            {
-              key: TabsKey.Payment,
-              label: "2. Thanh toan",
-              children: <Payment />,
-            },
-          ];
-          return (
-            <Form>
-              <Tabs
-                activeKey={tab ?? TabsKey.Info}
-                items={items.map((item) => ({
-                  ...item,
-                  label: (
-                    <span
-                      className={
-                        item.key === tab ? "color-default" : "text-[#919EAB]"
-                      }
-                      style={{
-                        padding: "0 80px",
-                        fontWeight: "600",
-                        fontSize: "18px",
-                      }}
-                    >
-                      {item.label}
-                    </span>
-                  ),
-                }))}
-                onChange={onChange}
-                centered
-              />
-              <div
-                className="bg-[#FFFFFF] max-h-[120px] min-h-[120px] fixed bottom-0 w-full z-10 min-w-[370px] max-w-[600px]"
-                style={{
-                  border: "1px solid rgba(145, 158, 171, .239)",
-                  boxShadow: "0 -4px 20px -1px rgba(40, 124, 234, .15)",
-                  borderTopRightRadius: "5px",
-                  borderTopLeftRadius: "5px",
-                  padding: "10px 10px 20px",
-                }}
-              >
-                <div className="flex justify-between">
-                  <span className="font-semibold">Tổng tiền tạm tính:</span>
-                  <span className="color-default font-bold">
-                    {conVertPrice(toTalPrice)}
-                  </span>
-                </div>
-                <button
-                  type="submit"
-                  className="bg-[#D70018] w-full mt-2 text-white rounded-[5px]"
-                  style={{ padding: ".375rem .75rem" }}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Formik initialValues={{}} onSubmit={onSubmit}>
+          {(props) => {
+            const { handleChange, values } = props;
+            const items: TabsProps["items"] = [
+              {
+                key: TabsKey.Info,
+                label: "1. Thong tin",
+                children: <InforPayment handleChange={handleChange} />,
+              },
+              {
+                key: TabsKey.Payment,
+                label: "2. Thanh toan",
+                children: <Payment />,
+              },
+            ];
+            return (
+              <Form>
+                <Tabs
+                  activeKey={tab ?? TabsKey.Info}
+                  items={items.map((item) => ({
+                    ...item,
+                    label: (
+                      <span
+                        className={
+                          item.key === tab ? "color-default" : "text-[#919EAB]"
+                        }
+                        style={{
+                          padding: "0 80px",
+                          fontWeight: "600",
+                          fontSize: "18px",
+                        }}
+                      >
+                        {item.label}
+                      </span>
+                    ),
+                  }))}
+                  onChange={onChange}
+                  centered
+                />
+                <div
+                  className="bg-[#FFFFFF] max-h-[120px] min-h-[120px] fixed bottom-0 w-full z-10 min-w-[370px] max-w-[600px]"
+                  style={{
+                    border: "1px solid rgba(145, 158, 171, .239)",
+                    boxShadow: "0 -4px 20px -1px rgba(40, 124, 234, .15)",
+                    borderTopRightRadius: "5px",
+                    borderTopLeftRadius: "5px",
+                    padding: "10px 10px 20px",
+                  }}
                 >
-                  Thanh toán
-                </button>
-                {tab === TabsKey.Payment && (
-                  <p className="text-[#4070F4] text-center w-full mt-[5px]">
-                    Kiểm tra danh sách sản phẩm
-                  </p>
-                )}
-              </div>
-            </Form>
-          );
-        }}
-      </Formik>
+                  <div className="flex justify-between">
+                    <span className="font-semibold">Tổng tiền tạm tính:</span>
+                    <span className="color-default font-bold">
+                      {conVertPrice(toTalPrice)}
+                    </span>
+                  </div>
+                  <button
+                    type="submit"
+                    className="bg-[#D70018] w-full mt-2 text-white rounded-[5px]"
+                    style={{ padding: ".375rem .75rem" }}
+                  >
+                    Thanh toán
+                  </button>
+                  {tab === TabsKey.Payment && (
+                    <p className="text-[#4070F4] text-center w-full mt-[5px]">
+                      Kiểm tra danh sách sản phẩm
+                    </p>
+                  )}
+                </div>
+              </Form>
+            );
+          }}
+        </Formik>
+      </Suspense>
     </div>
   );
 };
